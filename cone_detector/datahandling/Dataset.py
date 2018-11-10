e059894562c123c964b000cf83a2bc94a7a6466e
nico <nicoloderita@gmail.com>
2018-11-10 19:59:35 +0100

    fixed emptyfolder need

--- cone_detector/datahandling/Dataset.py
+++ cone_detector/datahandling/Dataset.py
@@ -18,53 +18,56 @@ class Dataset(object):
 
         if particular_dir is not None:
             self.annotations_dir = particular_dir
+        try:
+            for ann in os.listdir(self.annotations_dir):
+                img = {'object': []}
 
-        for ann in os.listdir(self.annotations_dir):
-            img = {'object': []}
+                tree = ET.parse(self.annotations_dir + ann)
 
-            tree = ET.parse(self.annotations_dir + ann)
+                for elem in tree.iter():
+                    if 'filename' in elem.tag:
+                        img_anns += [img]
+                        img['filename'] = elem.text
+                        # img['image_data'] = self.read_image_from_file(img['filename'])
+                    if 'width' in elem.tag:
+                        img['width'] = int(elem.text)
+                    if 'height' in elem.tag:
+                        img['height'] = int(elem.text)
+                    if 'object' in elem.tag or 'part' in elem.tag:
+                        obj = {}
 
-            for elem in tree.iter():
-                if 'filename' in elem.tag:
-                    img_anns += [img]
-                    img['filename'] = elem.text
-                    # img['image_data'] = self.read_image_from_file(img['filename'])
-                if 'width' in elem.tag:
-                    img['width'] = int(elem.text)
-                if 'height' in elem.tag:
-                    img['height'] = int(elem.text)
-                if 'object' in elem.tag or 'part' in elem.tag:
-                    obj = {}
+                        for attr in list(elem):
+                            if 'name' in attr.tag:
+                                obj_name = attr.text
+                                obj['name'] = obj_name
+                                try:
+                                    classes[obj_name] = classes[obj_name] + 1
+                                except KeyError:
+                                    log.info("New class found in dataset: {}".format(obj_name))
+                                    classes[obj_name] = 1
 
-                    for attr in list(elem):
-                        if 'name' in attr.tag:
-                            obj_name = attr.text
-                            obj['name'] = obj_name
-                            try:
-                                classes[obj_name] = classes[obj_name] + 1
-                            except KeyError:
-                                log.info("New class found in dataset: {}".format(obj_name))
-                                classes[obj_name] = 1
+                                # add additional label if class label available
+                                #if obj_name in self.parameters.labels_dict:
+                                #    obj['class'] = self.parameters.labels_dict[obj['name']]
+                                img['object'] += [obj]
 
-                            # add additional label if class label available
-                            #if obj_name in self.parameters.labels_dict:
-                            #    obj['class'] = self.parameters.labels_dict[obj['name']]
-                            img['object'] += [obj]
-
-                        if 'bndbox' in attr.tag:
-                            for dim in list(attr):
-                                if 'xmin' in dim.tag:
-                                    obj['xmin'] = int(round(float(dim.text)))
-                                if 'ymin' in dim.tag:
-                                    obj['ymin'] = int(round(float(dim.text)))
-                                if 'xmax' in dim.tag:
-                                    obj['xmax'] = int(round(float(dim.text)))
-                                if 'ymax' in dim.tag:
-                                    obj['ymax'] = int(round(float(dim.text)))
+                            if 'bndbox' in attr.tag:
+                                for dim in list(attr):
+                                    if 'xmin' in dim.tag:
+                                        obj['xmin'] = int(round(float(dim.text)))
+                                    if 'ymin' in dim.tag:
+                                        obj['ymin'] = int(round(float(dim.text)))
+                                    if 'xmax' in dim.tag:
+                                        obj['xmax'] = int(round(float(dim.text)))
+                                    if 'ymax' in dim.tag:
+                                        obj['ymax'] = int(round(float(dim.text)))
+        except FileNotFoundError:
+            log.info("The folder: {} does not exists => skipping this dataset folder".format(self.annotations_dir))
+            return None
 
         for dataset_class in classes:
             if dataset_class not in self.parameters.labels_list:
-                raise ValueError("Class {} found in dataset doesn't have a mach in labels_dict.".format(dataset_class))
+                raise ValueError("Class {} found in dataset doesn't have a mach in labels_list.".format(dataset_class))
 
         if len(classes) != len(self.parameters.labels_list):
             log.warn("Len of found classes in dataset {} different from provided labels_dict {}".format(len(classes),
